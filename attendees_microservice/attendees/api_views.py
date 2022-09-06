18e15ec7c746ead9dfe9f07a1d1d714e5fca0226
Christopher <chris.hem10@gmail.com>
2022-09-06 12:35:43 -0500

    Updates attendees and react.

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -15,6 +15,9 @@ class AttendeeListEncoder(ModelEncoder):
     model = Attendee
     properties = ["name"]
 
+    def get_extra_data(self, o):
+        return {"conference": o.conference.name}
+
 
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
@@ -56,7 +59,10 @@ def api_list_attendees(request, conference_vo_id=None):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        if conference_vo_id is not None:
+            attendees = Attendee.objects.filter(conference=conference_vo_id)
+        else:
+            attendees = Attendee.objects.all()
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -82,7 +88,7 @@ def api_list_attendees(request, conference_vo_id=None):
             safe=False,
         )
 
-
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_attendee(request, pk):
     """
     Returns the details for the Attendee model specified
@@ -103,9 +109,35 @@ def api_show_attendee(request, pk):
         }
     }
     """
-    attendee = Attendee.objects.get(id=pk)
-    return JsonResponse(
-        attendee,
-        encoder=AttendeeDetailEncoder,
-        safe=False,
-    )
+    if request.method == "GET":
+        attendee = Attendee.objects.get(id=pk)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Attendee.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            try:
+                if "conference" in content:
+                    conference = ConferenceVO.objects.get(
+                        id=content["conference"]
+                    )
+                    content["conference"] = conference
+            except ConferenceVO.DoesNotExist:
+                return JsonResponse(
+                    {"message": "Invalid conference id"},
+                    status=400
+                )
+            Attendee.objects.filter(id=pk).update(**content)
+            attendee = Attendee.objects.get(id=pk)
+            return JsonResponse(
+                attendee, encoder=AttendeeDetailEncoder,
+                safe=False
+            )
+        except Attendee.DoesNotExist:
+            return JsonResponse({"message": "Invalid attendee id"})
